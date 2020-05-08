ccd41bafe375ec3f3e29731d33db9612c320d5cd
KSClopton <kyle.clopton@outlook.com>
2020-05-07 21:05:28 -0400

    finished all tasks

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,9 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-axios.get('https://api.github.com/users/KSCLopton')
+
+
+// axios.get('https://api.github.com/users/KSCLopton')
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -12,11 +14,74 @@ axios.get('https://api.github.com/users/KSCLopton')
     Skip to STEP 3.
 */
 
+function makeGitCard(attrs){
+
+
+  // creating elements
+  const cardDiv = document.createElement('div')
+  const cardImg = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const nameHead = document.createElement('h3')
+  const userName = document.createElement('p')
+  const userLocation = document.createElement('p')
+  const profile = document.createElement('p')
+  const profAddress = document.createElement('a')
+  const userFollowers = document.createElement('p')
+  const userFollowing = document.createElement('p')
+  const userBio = document.createElement('p')
+
+  // creating structure
+  cardDiv.appendChild(cardImg)
+  cardDiv.appendChild(cardInfo)
+  cardInfo.appendChild(nameHead)
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(userLocation)
+  cardInfo.appendChild(profile)
+  profile.appendChild(profAddress)
+  cardInfo.appendChild(userFollowers)
+  cardInfo.appendChild(userFollowing)
+  cardInfo.appendChild(userBio)
+
+  // adding classList and text content
+  // cardDiv.classList.add('card')
+  // cardInfo.classList.add('card-info')
+  // nameHead.classList.add('name')
+  // userName.classList.add('username')
+  userFollowers.textContent = `Followers: ${attrs.followers}`
+  userFollowing.textContent = `Following: ${attrs.following}`
+  userBio.textContent = `Bio: ${attrs.bio}`
+  profAddress.href = attrs.html_url
+  cardImg.src = attrs.avatar_url
+  nameHead.textContent = attrs.name
+  userName.textContent = attrs.login
+  userLocation.textContent = attrs.location
+  profAddress.textContent = attrs.html_url
+
+  cardDiv.classList.add('card')
+  cardInfo.classList.add('card-info')
+  nameHead.classList.add('name')
+  userName.classList.add('username')
+  
+  return cardDiv
+  }
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-makeFunction
+
+function getGitHub(username){
+  axios.get(`https://api.github.com/users/${username}`)
+    .then(response => {
+      const cards = document.querySelector('.cards')
+      const markup = response.data
+        const user = makeGitCard(markup)
+      cards.appendChild(user)
+})
+    .then()
+}
+
+
+
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -27,9 +92,15 @@ makeFunction
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
+getGitHub('KSClopton');
 
-const followersArray = [];
-
+const followersArray = ['JDMTias','meep-morp', 'garybot','barbaralois','tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+function findFriends (friendsArr){
+  for(let i = 0; i < friendsArr.length; i++){
+    getGitHub(friendsArr[i])
+  }
+}
+findFriends(followersArray);
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -50,54 +121,6 @@ const followersArray = [];
     </div>
 */
 
-
-function makeFunction({attrs}){
-const {avatar_url, name, login, location, html_url, followers, following, bio} = attrs;
-
-const cardDiv = document.createElement('div')
-const cardImg = document.createElement('img')
-const cardInfo = document.createElement('div')
-const nameHead = document.createElement('h3')
-const userName = document.createElement('p')
-const userLocation = document.createElement('p')
-const profile = document.createElement('p')
-const profAddress = document.createElement('a')
-const userFollowers = document.createElement('p')
-const userFollowing = document.createElement('p')
-const userBio = document.createElement('p')
-
-cardDiv.appendChild(cardImg)
-cardDiv.appendChild(cardInfo)
-cardInfo.appendChild(nameHead)
-cardInfo.appendChild(userName)
-cardInfo.appendChild(userLocation)
-cardInfo.appendChild(profile)
-profile.appendChild(profAddress)
-cardInfo.appendChild(userFollowers)
-cardInfo.appendChild(userFollowing)
-cardInfo.appendChild(userBio)
-
-cardDiv.classList.add('card')
-cardInfo.classList.add('card-info')
-nameHead.classList.add('name')
-userName.classList.add('username')
-
-
-cardImg.src = avatar_url
-nameHead.textContent = name
-userName.textContent = login
-userLocation.textContent = location
-profAddress.href = html_url
-profAddress.textContent = html_url
-userFollowers.textContent = followers
-userFollowing.textContent = following
-userBio.textContent = bio
-
-
-
-
-
-}
 /*
   List of LS Instructors Github username's:
     tetondan
